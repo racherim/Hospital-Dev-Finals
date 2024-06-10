afe1584e0499f5d7be0da1afa588cc4cc59b1306
Meykol <116001569+Meykol@users.noreply.github.com>
2024-06-11 01:25:30 +0800

    updates

--- Database Files/Insertions.sql
+++ Database Files/Insertions.sql
@@ -31,62 +31,40 @@ INSERT INTO LoginTable(Email, Password, Type) VALUES('Jheysen@gmail.com', 'docto
 INSERT INTO LoginTable(Email, Password, Type) VALUES('Romel@gmail.com', 'doctor', 2)
 INSERT INTO LoginTable(Email, Password, Type) VALUES('Michael@gmail.com', 'doctor',2)
 
-INSERT INTO LoginTable(Email, Password, Type) VALUES('ABC@gmail.com', 'abc', 1)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('DEF@gmail.com', 'abc', 1)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('XYZ@gmail.com', 'abc', 1)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('Kean@gmail.com', 'abc', 1)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('JipDaper@gmail.com', 'abc', 1)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('Rigor@gmail.com', 'abc', 1)
 
 
 
 --DOCTOR INSERTIONS
 DECLARE @ID INT
 SELECT @ID = LoginID FROM LoginTable WHERE Email = 'Aaron@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Aaron Rodriguez', '09209316496', 'Enjoy, Lahore', '01-01-2000', 'M', 1, 2500, 30000, 4, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+INSERT INTO Doctor VALUES(@ID, 'Aaron Rodriguez', '09209316496', 'Malabon', '01-01-2000', 'M', 1, 2500, 30000, 4, 0, 'United States Medical Licensing Examination, Bachelor Degree ', 'ENJOY', 10, 1)
 SELECT @ID = LoginID FROM LoginTable WHERE Email = 'Jheysen@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Jheysen Pacheco', '09649947296', 'Enjoy, Lahore', '02-02-2002', 'M', 1, 3000, 25000, 3.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+INSERT INTO Doctor VALUES(@ID, 'Jheysen Pacheco', '09649947296', 'Caloocan', '02-02-2002', 'M', 1, 3000, 25000, 3.5, 0, 'United States Medical Licensing Examination, Bachelor Degree', 'ENJOY', 10, 1)
 SELECT @ID = LoginID FROM LoginTable WHERE Email = 'Romel@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Romel Osorio', '09985948196', 'Enjoy, Lahore', '03-03-2003', 'M', 1, 1500, 20000, 5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+INSERT INTO Doctor VALUES(@ID, 'Romel Osorio', '09985948196', 'Navotas', '03-03-2003', 'M', 1, 1500, 20000, 5, 0, 'United States Medical Licensing Examination, Bachelor Degree', 'ENJOY', 10, 1)
 SELECT @ID = LoginID FROM LoginTable WHERE Email = 'Michael@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Michael Ubarco', '09978948197', 'Enjoy, Lahore', '04-04-2004', 'M', 1, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+INSERT INTO Doctor VALUES(@ID, 'Michael Ubarco', '09978948197', 'Bulacan', '04-04-2004', 'M', 1, 1000, 15000, 4.5, 0, 'United States Medical Licensing Examination, Bachelor Degree', 'ENJOY', 10, 1)
 
 --PATIENT INSERTIONS
 DECLARE @P_ID INT
-SELECT @P_ID = LoginID FROM LoginTable WHERE Email='ABC@gmail.com'
-INSERT INTO Patient VALUES(@P_ID, 'ABC', '61536516', 'ENJOY, LAHORE', '4-4-1996', 'M')
-SELECT @P_ID = LoginID FROM LoginTable WHERE Email='DEF@gmail.com'
-INSERT INTO Patient VALUES(@P_ID, 'DEF', '61536516', 'ENJOY, LAHORE', '4-4-1996', 'M')
-SELECT @P_ID = LoginID FROM LoginTable WHERE Email='XYZ@gmail.com'
-INSERT INTO Patient VALUES(@P_ID, 'XYZ', '61536516', 'ENJOY, LAHORE', '4-4-1996', 'M')
+SELECT @P_ID = LoginID FROM LoginTable WHERE Email='Kean@gmail.com'
+INSERT INTO Patient VALUES(@P_ID, 'Kean Lura', '09098989874', 'Caloocan', '4-4-1996', 'M')
+SELECT @P_ID = LoginID FROM LoginTable WHERE Email='JipDaper@gmail.com'
+INSERT INTO Patient VALUES(@P_ID, 'Jip Dampher', '09878456241', 'Caloocan', '1-6-1996', 'M')
+SELECT @P_ID = LoginID FROM LoginTable WHERE Email='Rigor@gmail.com'
+INSERT INTO Patient VALUES(@P_ID, 'Rigor Syguat', '09874523614', 'Malabon', '1-1-1999', 'M')
 
 
 select * from OtherStaff
 
-insert into OtherStaff values ('Javed', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Hamza', '03227561002','Iqbal Town, Lhr', 'Sweeper', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Kashan', '03227561002','Iqbal Town, Lhr', 'Security', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Alio', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Kaleem', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Ali', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Charles', '09209316496','Lambakin Bulacan', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Jeff', '09098989874','Saog Marilao', 'Sweeper', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Kesha', '09874523614','Iba Lambakin', 'Security', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Rigor', '09098989874','Caloocan North', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Lura', '09978948197','Monumento', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Kean', '09649947296','Navotas', 'Guard', 'M', '05-04-1990', 'Matric',5000)
 
-
-
-
-
-
---Because I have inserted the date and time in a particular format in the appointment table and that format has to be maintained
---for proper working
-
-/*
---APPOINTMENT INSERTIONS 
-DECLARE @DOCTOR_ID INT
-DECLARE @PATIENT_ID INT
-SELECT @DOCTOR_ID = LoginID FROM LoginTable WHERE Email='farhan@gmail.com'
-SELECT @PATIENT_ID = LoginID FROM LoginTable WHERE Email='ABC@gmail.com'
-INSERT INTO Appointment(DoctorID, PatientID, Date, Appointment_Status) VALUES(@DOCTOR_ID, @PATIENT_ID, '2017-5-4 10:00:00', 3)
-SELECT @DOCTOR_ID = LoginID FROM LoginTable WHERE Email='farhan@gmail.com'
-SELECT @PATIENT_ID = LoginID FROM LoginTable WHERE Email='DEF@gmail.com'
-INSERT INTO Appointment(DoctorID, PatientID, Date, Appointment_Status) VALUES(@DOCTOR_ID, @PATIENT_ID, '2017-5-4 12:00:00', 1)
-SELECT @DOCTOR_ID = LoginID FROM LoginTable WHERE Email='hassaan@gmail.com'
-SELECT @PATIENT_ID = LoginID FROM LoginTable WHERE Email='DEF@gmail.com'
-INSERT INTO Appointment(DoctorID, PatientID, Date, Appointment_Status) VALUES(@DOCTOR_ID, @PATIENT_ID, '2017-5-4 16:00:00', 3)
-*/
 
